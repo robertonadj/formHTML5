1c4afca599a8fb88bdd2313a3992ce935e094c03
Roberto Nadj <robertonadj@yahoo.com.br>
2021-02-24 20:06:21 -0300

    adicionada função para calcular valores totais

--- app.js
+++ app.js
@@ -20,7 +20,45 @@
     };
     
     saveBtn.addEventListener('click', saveForm, false);
+
+    // função para cálculo de valores totais.
+    var qtyFields = orderForm.quantity,
+    totalFields = document.getElementsByClassName('item_total'),
+    orderTotalField = document.getElementById('order_total');
+
+    var formatMoney = function(value) {
+        return value.toString().replace(/\B(?=(\d{3}) + (?!\d))/g, ",");
+    }
+
+    var calculateTotals = function() {
+        var i = 0,
+        ln = qtyFields.length,
+        itemQty = 0,
+        itemPrice = 0.00,
+        itemTotal = 0.00,
+        itemTotalMoney = '$0.00',
+        orderTotal = 0.00,
+        orderTotalMoney = '$0.00';
+
+        for(; i<ln; i++) {
+    }
 };
+
+calculateTotals();
+
+var qtyListeners = function() {
+    var i = 0,
+    ln = qtyFields.length;
+
+    for(; i<ln; i++) {
+        qtyFields[i].addEventListener('input', calculateTotals, false);
+        qtyFields[i].addEventListener('keyup', calculateTotals, false);
+    }
+};
+
+qtyListeners();
+
+}
 
 window.addEventListener('load', init, false);
 
