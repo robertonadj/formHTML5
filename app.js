491df25c194f0e5d772862469cba0a8331672ff9
Roberto Nadj <robertonadj@yahoo.com.br>
2021-03-08 19:23:38 -0300

    pequenos ajustes no form

--- app.js
+++ app.js
@@ -68,9 +68,10 @@
                     totalFields[i].innerHTML = itemTotalMoney;
                     orderTotalField.innerHTML = orderTotalMoney;
                 }
-            }
-    }
+        }
+    
 };
+
 
 calculateTotals();
 
